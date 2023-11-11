ea5473a6471eda0d8574abe6d91bc9ffcb73b471
Niklock <nikita.mitin.04@mail.ru>
2023-11-11 22:56:15 +0300

    Start project, new changes second

--- WATER_website/WATER_website/urls.py
+++ WATER_website/WATER_website/urls.py
@@ -7,4 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('', include('about_us.urls')),
+    path('', include('products.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
