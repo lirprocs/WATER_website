3749f675ab94f8a50e53a4809ae107af88bf94ac
Niklock <nikita.mitin.04@mail.ru>
2023-11-20 20:07:16 +0300

    success send to telegram

--- WATER_website/products/views.py
+++ WATER_website/products/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 import json
 from django.http import JsonResponse
+from aiogram import types, Dispatcher, Bot
+from aiogram.types import Update
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+
 
 def products(request):
     return render(request, 'products/products.html')
+
+async def telegram_message(nickname, phone, adress):
+
+    bot = Bot("6747649824:AAEfNPhBSF3DZ9jL9P4AFfN_StAeKC21q1Q")
+    storage = MemoryStorage()
+    dispatcher = Dispatcher(storage=storage)
+    chat_id = '281887511'
+    await bot.send_message(chat_id, f"New order:\nName: {nickname} \nPhone: {phone} \nAdress: {adress}")
+
+async def send_telegram_message(nickname, phone, adress):
+    storage = MemoryStorage()
+    dispatcher = Dispatcher(storage=storage)
+    await telegram_message(nickname, phone, adress)
 
 
 def sendMessage(request):
@@ -11,16 +29,9 @@ def sendMessage(request):
         data = json.loads(request.body)
         nickname = data.get('nickname')
         phone = data.get('phone')
-        address = data.get('adress')
+        adress = data.get('adress')
+        asyncio.run(send_telegram_message(nickname, phone, adress))
 
         return JsonResponse({'message': 'Сообщение получено и обработано успешно!'}, status=200)
     else:
         return JsonResponse({'error': 'Метод не разрешен'}, status=405)
-
-
-
-
-
-
-
-
--- WATER_website/static/js/script.js
+++ WATER_website/static/js/script.js
@@ -13,6 +13,10 @@ function sendMessage(event) {
     const phone = document.getElementById('phone').value;
     const adress = document.getElementById('adress').value;
 
+    if (nickname.trim() === '' || phone.trim() === '' || adress.trim() === '') {
+      document.getElementById("Wasnt sent").style.display = 'block';
+    }
+
     const xhr = new XMLHttpRequest();
 
     xhr.open('POST', 'products/sendMessage', true);
